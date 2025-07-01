21f3d1a6b65f8c2c89c065e4b49dd67297fec209
unknown <david.jung@uniat.edu.mx>
2025-07-01 15:19:57 -0600

    first commit

---
+++ Assets/Scripts/GameManagement/EconomyManager.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+/*
+	Documentation: https://mirror-networking.gitbook.io/docs/guides/networkbehaviour
+	API Reference: https://mirror-networking.com/docs/api/Mirror.NetworkBehaviour.html
+*/
+
+public class EconomyManager : NetworkBehaviour
+{
+    public static EconomyManager singleton;
+
+    private List<Player> players = new List<Player>();
+
+    #region Unity Callbacks
+    protected override void OnValidate()
+    {
+        base.OnValidate();
+    }
+
+    private void Awake()
+    {
+        if (singleton != null && singleton != this) { Destroy(this); } else { singleton = this; }
+    }
+
+    public void RegisterPlayer(Player newPlayer)
+    {
+        players.Add(newPlayer);
+    }
+
+    void Start()
+    {
+
+    }
+
+    #endregion
+}
